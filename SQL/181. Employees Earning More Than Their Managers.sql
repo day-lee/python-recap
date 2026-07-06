adcbdc9258f08bf1cbe6a4cb598e8fa41c839cb3
Dayeon Lee <iamdayeonlee@gmail.com>
2026-07-06 21:11:14 +0100

    self join

--- SQL/181. Employees Earning More Than Their Managers.sql
+++ SQL/181. Employees Earning More Than Their Managers.sql
@@ -1,11 +1,12 @@
+https://leetcode.com/problems/employees-earning-more-than-their-managers/description/
 - 셀프 조인 
 
 - 메인 테이블은 직원의 정보 테이블이고, 추가적으로 알고자하는 매니저 테이블을 셀프 조인한다. 
 - 직원의 매니저 id와 그의 실제 id를 매칭시켜서 조인한다. 
 - 직원의 매니저 id 단서를 가지고 매니저 테이블의 실제 id와 매칭시키고자 한다. 
 
-select em.name as Employee
-from employee em
+select e.name as Employee
+from employee e
 join employee m 
-on em.manager_id = m.id 
-where em.salary > m.salary
+on e.manager_id = m.id 
+where e.salary > m.salary
