48131057120a95c1cc78da490ab8ae8486dd5e5b
Dayeon Lee <iamdayeonlee@gmail.com>
2026-07-07 11:12:35 +0100

    I like CTE

---
+++ SQL/1327.list-the-products-ordered-in-a-period.sql
@@ -0,0 +1,14 @@
+https://leetcode.com/problems/list-the-products-ordered-in-a-period
+
+
+-- cte로 나눠서 풀었음. inner join 바로 연결해서 풀어도 되지만 가독성이나 성능면에서 cte가 나음 
+
+with result as (select product_id, sum(unit) as unit
+from orders 
+where order_date between '2020-02-01' and '2020-02-29'
+group by product_id
+having sum(unit) >= 100)
+
+select p.product_name, r.unit  
+from result r
+join  products p on r.product_id = p.product_id
