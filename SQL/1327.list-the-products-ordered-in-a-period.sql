65ab38b5aa440f8294c37de1e87ed6760f91b968
Dayeon Lee <iamdayeonlee@gmail.com>
2026-07-07 11:16:06 +0100

    caution: where between for date format is fine but time precision is not guarantted

--- SQL/1327.list-the-products-ordered-in-a-period.sql
+++ SQL/1327.list-the-products-ordered-in-a-period.sql
@@ -2,6 +2,8 @@ https://leetcode.com/problems/list-the-products-ordered-in-a-period
 
 
 -- cte로 나눠서 풀었음. inner join 바로 연결해서 풀어도 되지만 가독성이나 성능면에서 cte가 나음 
+-- whee between은 날짜 단위일때는 괜찮은데 시간이 붙으면 부등호를 붙이는게 정확함 
+-- WHERE order_date >= '2020-02-01' AND order_date < '2020-03-01'
 
 with result as (select product_id, sum(unit) as unit
 from orders 
