8f97327d57719b25cfc4fc855951c2f782149005
Dayeon Lee <iamdayeonlee@gmail.com>
2026-07-27 12:19:28 +0100

    select is for column, we can use number for in group by clause instead of lengthy queries

--- SQL/1907.count-salary-categories.sql
+++ SQL/1907.count-salary-categories.sql
@@ -1,9 +1,47 @@
 https://leetcode.com/problems/count-salary-categories/
 
 - 문제의 핵심은 데이터가 없는 카테고리도 결과에 0으로 반드시 포함해야 한다
-- COUNT(NULL)은 0을 반환
-- 집합의 결합: 컬럼을 동일하게 만들어주고, union으로 합쳐준다. 
+
+
 --
+CTE, JOIN, CASE WHEN THEN END 이용 - 모범 답안
+- 이미 다 계산 된 뒤에 필터링된 적은 로우에서 조인 부하가 가장 적음
+- 선집계 후 카테고리를 조인(3건 조인) 
+- group by 1은 첫번째 컬럼 category를 의미함. 
+
+| category       |
+| -------------- |
+| Low Salary     |
+| Average Salary |
+| High Salary    |
+
+WITH categories AS (
+    SELECT 'Low Salary' AS category
+    UNION ALL SELECT 'Average Salary'
+    UNION ALL SELECT 'High Salary'
+),
+account_counts AS (
+    SELECT 
+        CASE 
+            WHEN income < 20000 THEN 'Low Salary'
+            WHEN income BETWEEN 20000 AND 50000 THEN 'Average Salary'
+            ELSE 'High Salary'
+        END AS category,
+        COUNT(account_id) AS accounts_count
+    FROM Accounts
+    GROUP BY 1
+)
+SELECT 
+    c.category,
+    COALESCE(a.accounts_count, 0) AS accounts_count
+FROM categories c
+LEFT JOIN account_counts a ON c.category = a.category;
+
+
+--
+가독성 제일 좋지만 3번 풀 스캔해서 성능은 탈락 
+- `COUNT(NULL)은 0을 반환`
+- 집합의 결합: 컬럼을 동일하게 만들어주고, union으로 합쳐준다. 
 
 SELECT 'Low Salary' AS category, COUNT(account_id) AS accounts_count
 FROM Accounts
@@ -20,29 +58,6 @@ UNION
 SELECT 'High Salary' AS category, COUNT(account_id) AS accounts_count
 FROM Accounts
 WHERE income > 50000;
-
-
---
-left join 이용
-- 복잡함  
-
-WITH categories AS (
-    SELECT 'Low Salary' AS category
-    UNION ALL
-    SELECT 'Average Salary'
-    UNION ALL 
-    SELECT 'High Salary'
-)
-
-SELECT 
-    c.category, 
-    COUNT(a.account_id) AS accounts_count
-FROM categories c 
-LEFT JOIN accounts a 
-    ON (c.category = 'Low Salary' AND a.income < 20000)
-    OR (c.category = 'Average Salary' AND a.income BETWEEN 20000 AND 50000)
-    OR (c.category = 'High Salary' AND a.income > 50000)
-GROUP BY c.category;
 
 
 
