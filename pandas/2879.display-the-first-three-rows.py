90e2af6585c90a9e0652aad730235ceb2cf76ed9
Dayeon Lee <iamdayeonlee@gmail.com>
2026-08-01 07:59:18 +0100

    equivalent to limit

---
+++ pandas/2879.display-the-first-three-rows.py
@@ -0,0 +1,12 @@
+https://leetcode.com/problems/display-the-first-three-rows
+
+Pandas, comparison with SQL
+https://pandas.pydata.org/docs/getting_started/comparison/comparison_with_sql.html
+
+Intro - 10 minutes to pandas
+https://pandas.pydata.org/docs/user_guide/10min.html#min
+
+import pandas as pd 
+
+def selectFirstRows(employees: pd.DataFrame) -> pd.DataFrame:
+    return employees.head(3)
