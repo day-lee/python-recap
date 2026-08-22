4e63bf074eee0873e8ce2fb78bade7e7f419bf39
Dayeon Lee <iamdayeonlee@gmail.com>
2026-08-22 21:18:00 +0100

    running sum vs rolling, moving sum

--- SQL/1204.last-person-to-fit-in-the-bus.sql
+++ SQL/1204.last-person-to-fit-in-the-bus.sql
@@ -3,14 +3,18 @@ https://leetcode.com/problems/last-person-to-fit-in-the-bus
 aggregate window functions
 https://datalemur.com/sql-tutorial/sql-aggregate-window-functions
 
-누적 합 Running Sum (달리다 느낌이 아니라 계속 이어지다의 느낌 기찻길🚂)
+누적 합 Running Sum 
+(달리다 느낌이 아니라 계속 이어지다의 느낌 기찻길🚂)
 - 윈도우 함수로 누적합 구할 수 있음 
 - cte에서 turn 기준으로 정렬후 몸무게 누적합 컬럼추가 
 - 외부 쿼리에서 누적합이 1000 이하인 사람들 중 마지막 사람을 가져오기위해 
   누적합을 내림차순으로 정렬 후 limit 1로 마지막 사람 가져오기
 
-- Rolling sum, Moving sum은 이동합: 특정 윈도우에서 시간이 흐르면서 더해지는 것 e.g. 최근 7일간 방문자 수 합계 
+이동 합 Rolling sum, Moving sum 
 (rolling, moving은 일정한 크기의 관람차가 굴러가는 모양 🎡)
+- 특정 윈도우에서 시간이 흐르면서 더해지는 것 e.g. 최근 7일간 방문자 수 합계 
+
+==========================================================
 WITH rs AS (
     SELECT 
         person_name, 
@@ -24,7 +28,7 @@ ORDER BY runningsum DESC
 LIMIT 1;
 
 --
-Explanation: The folowing table is ordered by the turn for simplicity.
+Explanation: The following table is ordered by the turn for simplicity.
 +------+----+-----------+--------+--------------+
 | Turn | ID | Name      | Weight | Total Weight |
 +------+----+-----------+--------+--------------+
