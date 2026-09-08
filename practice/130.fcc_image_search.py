17df8e18372d3860163a11a8d421617add85aa7a
Dayeon Lee <iamdayeonlee@gmail.com>
2026-09-08 11:17:31 +0100

    Add thought process

--- practice/130.fcc_image_search.py
+++ practice/130.fcc_image_search.py
@@ -1,4 +1,7 @@
 # https://www.freecodecamp.org/learn/daily-coding-challenge/11-04
+- case-sensitive하므로 클렌징이 필요하다.
+- 비교를 위해서는 IN operator 사용 가능하다.
+
 def image_search(images, term):
     # r = []
     # for word in images:
